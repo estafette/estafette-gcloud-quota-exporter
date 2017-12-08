596a2f04f01a2fd148c0ea0f358239d67e9bd4c2
Jorrit Salverda <jsalverda@travix.com>
2017-12-08 13:56:32 +0100

    upgraded zerolog

--- govendor.sh
+++ govendor.sh
@@ -3,5 +3,5 @@ govendor fetch github.com/prometheus/client_golang/prometheus@v0.8.0
 govendor fetch github.com/prometheus/client_golang/prometheus/promhttp@v0.8.0
 govendor fetch github.com/stretchr/testify/assert@v1.1.4
 govendor fetch github.com/stretchr/testify/mock@v1.1.4
-govendor fetch github.com/rs/zerolog/log@v1.2.2
+govendor fetch github.com/rs/zerolog/log@v1.3.0
 govendor fetch github.com/GoogleCloudPlatform/google-cloud-go@v0.16.0
